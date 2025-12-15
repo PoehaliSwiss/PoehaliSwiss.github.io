255afae460961106c74d245aca1bfcfa0c6314b7
PoehaliSwiss <123464621+PoehaliSwiss@users.noreply.github.com>
2025-12-15 11:21:11 +0100

    Delete src directory

--- src/vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '*.mdx' {
-    import type { ComponentType, PropsWithChildren } from 'react'
-    const component: ComponentType<PropsWithChildren>
-    export default component
-    export const frontmatter: Record<string, any>
-}
